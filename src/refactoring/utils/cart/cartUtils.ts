1a22b7bcf400fff111c8a14ecdc75bb71dc73076
hjnoh <hyunjin.noh@saltlux.com>
2024-10-22 23:09:48 +0900

    [refactor] cartUtil 파일 경로 수정

--- src/refactoring/utils/cart/cartUtils.ts
+++ src/refactoring/utils/cart/cartUtils.ts
@@ -1,4 +1,4 @@
-import { CartItem, Coupon, DiscountType } from '../../types';
+import { CartItem, Coupon, DiscountType, Product } from '../../../types';
 
 export const calculateItemTotal = (item: CartItem) => {
   const { price } = item.product;
@@ -87,4 +87,27 @@ export const updateCartItemQuantity = (
       : item
   );
   return updatedCart;
+};
+
+export const getMaxDiscount = (
+  discounts: { quantity: number; rate: number }[]
+) => {
+  return discounts.reduce((max, discount) => Math.max(max, discount.rate), 0);
+};
+
+export const getRemainingStock = (product: Product, cart: CartItem[]) => {
+  const cartItem = cart.find((item) => item.product.id === product.id);
+  return product.stock - (cartItem?.quantity || 0);
+};
+
+export const getAppliedDiscount = (item: CartItem) => {
+  const { discounts } = item.product;
+  const { quantity } = item;
+  let appliedDiscount = 0;
+  for (const discount of discounts) {
+    if (quantity >= discount.quantity) {
+      appliedDiscount = Math.max(appliedDiscount, discount.rate);
+    }
+  }
+  return appliedDiscount;
 };
